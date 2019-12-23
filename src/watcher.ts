4439082b1b2be4eb2cbc543977a5345f185cff18
chalin <18875906195@163.com>
2019-12-23 18:58:20 +0800

    :construction: WIP, work in progress

--- src/watcher.ts
+++ src/watcher.ts
@@ -4,29 +4,29 @@ import { RedisConnection } from './redis';
 export default class RedisWatcher implements Watcher {
   private pubConnection!: RedisConnection;
   private subConnection!: RedisConnection;
-  private callback!: () => void;
+  private callback: () => void;
 
-  constructor (uri: string) {
-    this.pubConnection = new RedisConnection(uri);
-    this.subConnection = new RedisConnection(uri);
-  }
-
-  static async newWatcher (uri: string): Promise<RedisWatcher> {
+  public static async newWatcher(uri: string): Promise<RedisWatcher> {
     const watcher = new RedisWatcher(uri);
     await Promise.all([watcher.pubConnection.open(), watcher.subConnection.open()]);
     return watcher;
   }
 
-  public async update (): Promise<boolean> {
+  constructor(uri: string) {
+    this.pubConnection = new RedisConnection(uri);
+    this.subConnection = new RedisConnection(uri);
+  }
+
+  public async update(): Promise<boolean> {
     try {
       await this.pubConnection.redisClient.publish('change', 'casbin rules updated');
       return true;
-    } catch {
+    } catch (e) {
       return false;
     }
   }
 
-  public setUpdateCallback (callback: () => void) {
+  public setUpdateCallback(callback: () => void) {
     this.callback = callback;
   }
 
