c8cb11bef6fb1cf16f6b19b9d90bd55380b8a7f0
Zixuan Liu <nodeces@gmail.com>
2020-11-19 17:44:58 +0800

    feat: support Redis cluster
    Signed-off-by: Zixuan Liu <nodeces@gmail.com>

--- src/redis.ts
+++ src/redis.ts
@@ -1,19 +1,48 @@
-import Redis, { RedisOptions } from 'ioredis';
+import Redis, { RedisOptions, Cluster, ClusterNode, ClusterOptions } from 'ioredis';
 
-export class RedisConnection {
+export type RedisClient = Redis.Cluster | Redis.Redis;
+
+export interface Connection {
+  close(): void;
+
+  getRedisClient(): Promise<RedisClient>;
+}
+
+export class RedisConnection implements Connection {
   private readonly options?: Redis.RedisOptions | string;
-  public redisClient: Redis.Redis;
+  private redisClient: Redis.Redis;
 
   constructor(options?: RedisOptions | string) {
     this.options = options;
-  }
-
-  public open() {
-    // @ts-ignore
+    // @ts-ignore - it works fine.
     this.redisClient = new Redis(this.options);
   }
 
   public close() {
     this.redisClient.disconnect();
+  }
+
+  public async getRedisClient(): Promise<Redis.Redis> {
+    return this.redisClient;
+  }
+}
+
+export class RedisClusterConnection implements Connection {
+  private readonly options?: Redis.ClusterOptions;
+  public redisClient: Redis.Cluster;
+  public nodes: ClusterNode[];
+
+  constructor(nodes: ClusterNode[] = [], options: ClusterOptions = {}) {
+    this.nodes = nodes;
+    this.options = options;
+    this.redisClient = new Redis.Cluster(this.nodes, this.options);
+  }
+
+  public close() {
+    this.redisClient.disconnect();
+  }
+
+  public async getRedisClient(): Promise<Redis.Cluster> {
+    return this.redisClient;
   }
 }
